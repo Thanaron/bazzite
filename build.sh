e2ced0e0cbf38b7b3cbfcff6f98dc3a16a4cd0e7
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-24 11:51:11 +0200

    add hyprland copr

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
+
 
 ### Install packages
 
