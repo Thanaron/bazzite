a811636ca25cc7be956491a4b26a2ed8f3063fd5
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-25 20:12:51 +0200

    add new packages

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ RELEASE="$(rpm -E %fedora)"
 
 curl --output-dir "/etc/yum.repos.d/" --remote-name \
     "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
+
+curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm"
+
+curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://downloads.1password.com/linux/rpm/stable/x86_64/1password-cli-latest.x86_64.rpm"
+
+rpm-ostree install \
+            1password \
+            1password-cli \
+            papirus-icon-theme \
+            rsms-inter-fonts
