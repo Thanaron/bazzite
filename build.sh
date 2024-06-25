e68c9061e677bb30af8488d1edb75ca3f179ef57
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-25 22:05:50 +0200

    remove anything related to 1password

--- build.sh
+++ build.sh
@@ -7,18 +7,6 @@ RELEASE="$(rpm -E %fedora)"
 curl --output-dir "/etc/yum.repos.d/" --remote-name \
     "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
 
-curl -o /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
-
-cat > /etc/yum.repos.d/1password.repo << EOF
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/1password.asc
-EOF
-
 rpm-ostree install \
     papirus-icon-theme \
     rsms-inter-fonts
