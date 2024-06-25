3cedf45740bdd643a5735059de833963f8e2db9b
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-25 22:11:47 +0200

    next try

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ RELEASE="$(rpm -E %fedora)"
 curl --output-dir "/etc/yum.repos.d/" --remote-name \
     "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
 
+
+mkdir -p /var/opt
+wget -q https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
+sudo rpm -vih ./1password-latest.rpm
+wget -q https://downloads.1password.com/linux/rpm/stable/x86_64/1password-cli-latest.x86_64.rpm
+rpm-ostree install ./1password-cli-latest.x86_64.rpm
+
 rpm-ostree install \
     papirus-icon-theme \
     rsms-inter-fonts
