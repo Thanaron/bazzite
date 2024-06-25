dacbfdcc4e28e2465ff946646e3fd45629560fbb
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-25 10:51:50 +0200

    fix broken packages

--- build.sh
+++ build.sh
@@ -6,8 +6,3 @@ RELEASE="$(rpm -E %fedora)"
 
 curl --output-dir "/etc/yum.repos.d/" --remote-name \
     "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
-
-
-rpm-ostree install code \
-                   nu \
-                   
