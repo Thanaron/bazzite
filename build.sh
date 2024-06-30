64874655956721ed6eb2777516ed9486d39da689
Tobias Küchler <tobias-kuechler@outlook.com>
2024-06-30 12:45:46 +0200

    add custom xivlauncher

--- build.sh
+++ build.sh
@@ -4,16 +4,15 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-curl --output-dir "/etc/yum.repos.d/" --remote-name \
-    "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${RELEASE}/solopasha-hyprland-fedora-${RELEASE}.repo"
-
-
-mkdir -p /var/opt
-wget -q https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
-sudo rpm -i ./1password-latest.rpm
-wget -q https://downloads.1password.com/linux/rpm/stable/x86_64/1password-cli-latest.x86_64.rpm
-sudo rpm -i ./1password-cli-latest.x86_64.rpm
+for i in solopasha/hyprland rankyn/xivlauncher; do
+    MAINTAINER="${i%%/*}"
+    REPOSITORY="${i##*/}"
+    curl --output-dir "/etc/yum.repos.d/" --remote-name \
+    "https://copr.fedorainfracloud.org/coprs/${MAINTAINER}/${REPOSITORY}/repo/fedora-${RELEASE}/${MAINTAINER}-${REPOSITORY}-fedora-${RELEASE}.repo"
+done
+ 
 
 rpm-ostree install \
     papirus-icon-theme \
-    rsms-inter-fonts
+    rsms-inter-fonts \
+    xivlauncher
